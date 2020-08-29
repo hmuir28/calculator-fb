1850776d19b281c5a5400bf981bab2c8dc3ddb57
Harry Muir <harry182894@gmail.com>
2020-08-29 13:03:26 -0600

    Added Footer and made changes on styles for App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,9 +6,21 @@
     >
       <h1>Universidad Americana</h1>
     </v-app-bar>
-    <v-main :class="$style.mainContainer">
+    <v-main :class="$style.container">
       <CalculatorMainPanel />
     </v-main>
+
+    <v-footer
+      padless
+      :class="[$style.container, $style.footer]"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Author: Harry Muir</strong>
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
@@ -21,23 +33,23 @@ export default {
   components: {
     CalculatorMainPanel,
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
 <style module>
+.container {
+  background-color: #1a1a1d9e !important;
+  display: flex;
+}
+
+.footer div:first-of-type {
+  color: #ffffff;
+}
+
 .headerContainer div:first-of-type {
   background: #CD1627;
 }
 
 .headerContainer h1 {
   color: #ffffff;
-}
-
-.mainContainer {
-  background-color: #1a1a1d9e;
-  display: flex;
 }
 </style>
